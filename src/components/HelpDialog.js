b25b7912b3cc51929f0f097086b87517104369dd
termat <ter.matsuoka@gmail.com>
2022-11-23 01:08:48 +0900

    update

--- src/components/HelpDialog.js
+++ src/components/HelpDialog.js
@@ -66,9 +66,19 @@ export default function DataTableDialog(props) {
           <h1 style={{margin: "8px"}}>Potavi ： ポタリングした地域を3D地図で俯瞰するWebアプリです。</h1>
           <p style={{fontSize: "20px",margin: "8px"}}>自転車走行記録（GPSログ）を可視化し、ポタリング（自転車散歩）した地域を俯瞰して楽しむWebアプリです。<br />
             実際に走った地域を俯瞰して振り返ることで新たな発見を促します。<br />
-            また、サイクリングルートや地域の紹介に活用することで、サイクルツーリズムを推進します。<br />
-            <Button variant="contained" style={{margin:"10px"}} size="large" onClick={handleDialogClose}>　開　始　</Button>
+            また、サイクリングルートや地域の紹介に活用することで、サイクルツーリズムを推進します。<br /><br />
             </p>
+            <hr />
+            <div>
+						<h5 style={{fontSize: "14px",margin: "8px"}}>免責事項</h5>
+						<p style={{fontSize: "12px",margin: "8px"}}>
+							明示、暗黙を問わず本サイトの内容に関してはいかなる保証も適用しません。<br />
+							本サイトの利用により、何らかのトラブルや損失・損害等が生じた場合、本サイトの関係者は損害、損失に対していかなる場合も一切の責任を負いません。<br />
+							コンテンツの全ての情報について、内容の合法性・正確性・安全性等、あらゆる点において保証しません。<br />
+							リンクをしている外部サイトについて、一切、保証しません。<br />
+							事前の予告無く、コンテンツの提供を中止する可能性があります。<br />
+						</p>
+            </div>
           </Box>
         </div>
         <br />
