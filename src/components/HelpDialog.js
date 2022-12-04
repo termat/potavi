8592e7742b435da744c807c1c899bf715ade32d9
termat <ter.matsuoka@gmail.com>
2022-12-04 19:28:11 +0900

    update

--- src/components/HelpDialog.js
+++ src/components/HelpDialog.js
@@ -66,7 +66,7 @@ export default function DataTableDialog(props) {
           <h1 style={{margin: "6px"}}>Potavi ： ポタリングした地域を3D地図で俯瞰するWebアプリです。</h1>
           <p style={{fontSize: "18px",margin: "6px"}}>自転車走行記録(GPSログ)を可視化し、ポタリング(自転車散歩)した地域を俯瞰して眺めるWebアプリです。<br />
             地域を俯瞰映像で振り返ることで新たな発見を促します。<br />
-            また、ルート紹介に利用するで、サイクルツーリズムの推進に活用できます。<br />
+            また、ルート紹介に利用することで、サイクルツーリズム推進に活用できます。<br />
             <Button variant="contained" style={{margin:"10px"}} size="large" onClick={handleDialogClose}>　開　始　</Button>
             <Button variant="contained" style={{margin:"10px"}} size="large" target="_blank" component="a" href="/potavi/info.html">説明ページ</Button>
             <br />
